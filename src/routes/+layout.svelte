a0fe1d77ecbf5dc31732e0bc5c5163ccfd43dae8
Nakshatra Mukhopadhyay <nakshatra@mode.com>
2024-07-13 13:35:56 +0530

    Generate absolute URLs for OG images

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,14 @@
-<script>
+<script lang="ts">
 	import '../app.css';
 	import { page } from '$app/stores';
+
+	function getImageURL(origin: string) {
+		const x = new URL(origin);
+		x.pathname = '/images/og-home.jpeg';
+		return x.toString();
+	}
+
+	$: imageURL = getImageURL($page.url.origin);
 </script>
 
 <svelte:head>
@@ -10,10 +18,10 @@
 		content="Discover the versatility of Muze with 20 different visualizations answering questions about labour market exits in OECD countries."
 	/>
 	<meta property="og:type" content="website" />
-	<meta property="og:logo" content="/images/og-home.jpg" />
+	<meta property="og:logo" content={imageURL} />
 	<meta property="og:url" content={$page.url.toString()} />
 	<meta property="twitter:card" content="summary_large_image" />
-	<meta property="og:image" content="/images/og-home.jpeg" />
+	<meta property="og:image" content={imageURL} />
 	<meta property="og:site_name" content="One Dataset, Twenty Muze Visualizations" />
 	<meta property="og:title" content="One Dataset, Twenty Muze Visualizations" />
 	<meta
