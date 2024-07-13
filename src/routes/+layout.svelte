cc5de0506e581c3f61f39fe04d208feb9d5ff4f5
Nakshatra Mukhopadhyay <nakshatra@mode.com>
2024-07-13 14:38:13 +0530

    Simplify OG image absolute URL generation logic

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,27 +1,19 @@
 <script lang="ts">
 	import '../app.css';
 	import { page } from '$app/stores';
-
-	function getImageURL(origin: string) {
-		const x = new URL(origin);
-		x.pathname = '/images/og-home.jpeg';
-		return x.toString();
-	}
-
-	$: imageURL = getImageURL($page.url.origin);
 </script>
 
 <svelte:head>
 	<title>One Dataset, Twenty Muze Visualizations</title>
 	<meta
-		property="description"
+		name="description"
 		content="Discover the versatility of Muze with 20 different visualizations answering questions about labour market exits in OECD countries."
 	/>
 	<meta property="og:type" content="website" />
-	<meta property="og:logo" content={imageURL} />
+	<meta property="og:logo" content={`${$page.url.origin}/images/og-home.jpeg`} />
 	<meta property="og:url" content={$page.url.toString()} />
 	<meta property="twitter:card" content="summary_large_image" />
-	<meta property="og:image" content={imageURL} />
+	<meta property="og:image" content={`${$page.url.origin}/images/og-home.jpeg`} />
 	<meta property="og:site_name" content="One Dataset, Twenty Muze Visualizations" />
 	<meta property="og:title" content="One Dataset, Twenty Muze Visualizations" />
 	<meta
