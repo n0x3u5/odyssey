9dd2a301c3e6aa7151de4ae51d49875c9e8092dd
Nakshatra Mukhopadhyay <nakshatra@mode.com>
2024-07-16 21:23:57 +0530

    Change 20 to 8

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -45,7 +45,7 @@
 						<br />
 						DATASET,
 						<br />
-						<span class="text-indigo-500">TWENTY</span>
+						<span class="text-indigo-500">EIGHT</span>
 						<span class="font-extralight underline"><em>MUZE</em></span>
 						<br />
 						VISUALIZATIONS
@@ -53,7 +53,7 @@
 				</header>
 				<section>
 					<p>
-						Here are 20 different visualizations, powered by <strong>Muze</strong>, created from a
+						Here are 8 different visualizations, powered by <strong>Muze</strong>, created from a
 						single dataset on labour market exit ages in OECD countries by year. Each chart shows
 						the dataset from a different perspective, answering a different question.
 					</p>
