c40aa5c2c9f1f475f7515f229bd5dda692b09e80
Nakshatra Mukhopadhyay <nakshatra@mode.com>
2024-07-23 02:54:53 +0530

    Pixel perfect highlighted dotplot

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,48 +1,53 @@
 type Canvas = {
-  data: (data: unknown) => Canvas;
-  rows: (rows: Array<string>) => Canvas;
-  columns: (columns: Array<string>) => Canvas;
-  layers: (layers: Array<unknown>) => Canvas;
-  config: (config: Record<string, unknown>) => Canvas;
-  yAxes: () => Array<{ mount: () => SVGSVGElement }>;
-  color: (color: string) => Canvas;
-  composition: () => {
-    visualGroup: {
-      placeholderInfo: () => {
-        values: Array<
-          Array<{
-            source: () => {
-              _graphics: { rootSvg: { node: () => SVGSVGElement } };
-              layers: () => Array<{
-                mount: () => SVGGElement;
-                _points: Array<Array<{ data: Record<string, number> }>>;
-                axes: () => {
-                  x: { scale: () => { range: () => [number, number] } };
-                  y: { scale: () => (domainValue: number | undefined) => number };
-                };
-              }>;
-              _gridLines: Array<{ mount: () => SVGGElement }>;
-            };
-          }>
-        >;
-      };
-    };
-  };
+	data: (data: unknown) => Canvas;
+	rows: (rows: Array<string> | Array<Array<string>>) => Canvas;
+	columns: (
+		columns:
+			| Array<string | { field: string; as: 'discrete' | 'continuous' }>
+			| Array<Array<string | { field: string; as: 'discrete' | 'continuous' }>>
+	) => Canvas;
+	layers: (layers: Array<unknown>) => Canvas;
+	config: (config: Record<string, unknown>) => Canvas;
+	yAxes: () => Array<{ mount: () => SVGSVGElement }>;
+	color: (color: string) => Canvas;
+	detail: (detail: Array<string>) => Canvas;
+	composition: () => {
+		visualGroup: {
+			placeholderInfo: () => {
+				values: Array<
+					Array<{
+						source: () => {
+							_graphics: { rootSvg: { node: () => SVGSVGElement } };
+							layers: () => Array<{
+								mount: () => SVGGElement;
+								_points: Array<Array<{ data: Record<string, number> }>>;
+								axes: () => {
+									x: { scale: () => { range: () => [number, number] } };
+									y: { scale: () => (domainValue: number | undefined) => number };
+								};
+							}>;
+							_gridLines: Array<{ mount: () => SVGGElement }>;
+						};
+					}>
+				>;
+			};
+		};
+	};
 };
 
 type DataModel = {
-  getField: <T>(fieldName: string) => { data: () => Array<T>; minMax: () => [number, number] };
-  select: (criteria: { field: string; value: unknown; operator: 'eq' }) => DataModel;
-  sort: (fields: Array<[string, 'asc' | 'desc']>) => DataModel;
-  calculateVariable: (
-    {
-      name,
-      type,
-      defAggFn
-    }: { name: string; type: 'measure' | 'dimension'; defAggFn?: 'sum' | 'avg' },
-    fields: Array<string>,
-    fn: (...args: Array<unknown>) => unknown
-  ) => DataModel;
+	getField: <T>(fieldName: string) => { data: () => Array<T>; minMax: () => [number, number] };
+	select: (criteria: { field: string; value: unknown; operator: 'eq' }) => DataModel;
+	sort: (fields: Array<[string, 'asc' | 'desc']>) => DataModel;
+	calculateVariable: (
+		{
+			name,
+			type,
+			defAggFn
+		}: { name: string; type: 'measure' | 'dimension'; defAggFn?: 'sum' | 'avg' },
+		fields: Array<string>,
+		fn: (...args: Array<unknown>) => unknown
+	) => DataModel;
 };
 
 export type { Canvas, DataModel };
--- src/routes/viz/highlightdotplot/+page.svelte
+++ src/routes/viz/highlightdotplot/+page.svelte
@@ -4,12 +4,35 @@
 
 	let { data: pageData }: { data: PageData } = $props();
 
-	const schema = $derived(pageData.schema);
-	const data = $derived(pageData.data);
+	const schema = $derived(pageData.schema ?? []);
+	const data = $derived(pageData.data ?? [[]]);
+
+	let selectedCountry = $state('Mexico');
 </script>
 
-<main class="h-screen w-screen p-4">
-	{#await import('@viz/muze') then { default: muze }}
-		<HighlightDotPlot {muze} {schema} {data}></HighlightDotPlot>
-	{/await}
-</main>
+<div
+	class="prose m-auto size-full max-w-4xl prose-h1:mb-0 prose-h1:mt-4 prose-h2:mb-0 prose-h2:mt-2"
+>
+	<main class="flex size-full flex-col pb-2">
+		<h1>
+			DISTRIBUTION OF COUNTRIES BY (<span class="uppercase text-blue-500">{selectedCountry}</span>
+			vs. <span class="text-neutral-400">OTHERS</span>) AGE OF LABOUR MARKET EXIT BY DECADE,
+			1972-2022
+		</h1>
+		{#await import('@viz/muze') then { default: muze }}
+			{@const dataModel = new muze.DataModel(muze.DataModel.loadDataSync(data, schema)).sort([
+				['Country', 'asc']
+			])}
+			{@const countries = dataModel.getField('Country').uniques()}
+			<select
+				bind:value={selectedCountry}
+				class="max-w-max self-end rounded border border-gray-500 uppercase"
+			>
+				{#each countries as country}
+					<option value={country}>{country}</option>
+				{/each}
+			</select>
+			<HighlightDotPlot {muze} {dataModel} {selectedCountry}></HighlightDotPlot>
+		{/await}
+	</main>
+</div>
