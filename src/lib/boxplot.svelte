b023a268ec0082b2fbe054f206f741b9e7ea5e67
Nakshatra Mukhopadhyay <nakshatra@mode.com>
2024-07-24 03:48:32 +0530

    Real box plot

--- src/lib/boxplot.svelte
+++ src/lib/boxplot.svelte
@@ -11,15 +11,123 @@
 	let {
 		muze,
 		schema = [],
-		data = [[]]
+		data = [[]],
+		isAdvanced = false
 	}: {
 		muze: Muze;
 		schema: Array<{ name: string; type: 'dimension' | 'measure' }>;
 		data: Array<Array<unknown>>;
+		isAdvanced: boolean;
 	} = $props();
 
+	const boxOpacity = $derived(isAdvanced ? 1 : 0);
 	const DataModel = $derived(muze.DataModel);
 	const html = $derived(muze.Operators.html);
+	const share = $derived(muze.Operators.share);
+	const layerFactory = $derived(muze.layerFactory);
+	const layers = [
+		{
+			name: 'boxPoint',
+			mark: 'point',
+			className: 'boxPoints',
+			encoding: {
+				y: 'boxMark.encoding.y',
+				x: 'boxMark.encoding.x',
+				color: 'boxMark.encoding.pointColor',
+				size: 'boxMark.encoding.pointSize'
+			},
+			transition: { disabled: true }
+		},
+		{
+			name: 'maxTick',
+			mark: 'tick',
+			className: 'boxTicks',
+			encoding: {
+				y: 'boxMark.encoding.maxValue',
+				x: 'boxMark.encoding.x'
+			},
+			interactive: false,
+			transition: { disabled: true }
+		},
+		{
+			name: 'upperTick',
+			className: 'upper-tick',
+			mark: 'tick',
+			encoding: {
+				y: 'boxMark.encoding.quarter',
+				x: 'boxMark.encoding.x',
+				y0: 'boxMark.encoding.minValue'
+			},
+			interactive: false,
+			transition: { disabled: true }
+		},
+		{
+			name: 'upperBand',
+			mark: 'bar',
+			className: 'upperBand',
+			encoding: {
+				y: 'boxMark.encoding.thirdQuarter',
+				x: 'boxMark.encoding.x',
+				y0: 'boxMark.encoding.medianValue',
+				color: 'boxMark.encoding.color'
+			},
+			transform: {
+				type: 'identity'
+			},
+			interactive: false,
+			transition: { disabled: true }
+		},
+		{
+			name: 'meanTick',
+			mark: 'tick',
+			className: 'boxTicks',
+			encoding: {
+				y: 'boxMark.encoding.medianValue',
+				x: 'boxMark.encoding.x'
+			},
+			interactive: false,
+			transition: { disabled: true }
+		},
+		{
+			name: 'lowerBand',
+			mark: 'bar',
+			className: 'lowerBand',
+			encoding: {
+				y0: 'boxMark.encoding.medianValue',
+				x: 'boxMark.encoding.x',
+				y: 'boxMark.encoding.quarter',
+				color: 'boxMark.encoding.color'
+			},
+			transform: {
+				type: 'identity'
+			},
+			interactive: false,
+			transition: { disabled: true }
+		},
+		{
+			name: 'lowerTick',
+			mark: 'tick',
+			className: 'boxTicks',
+			encoding: {
+				y: 'boxMark.encoding.maxValue',
+				x: 'boxMark.encoding.x',
+				y0: 'boxMark.encoding.thirdQuarter'
+			},
+			interactive: false,
+			transition: { disabled: true }
+		},
+		{
+			name: 'minTick',
+			mark: 'tick',
+			className: 'boxTicks',
+			encoding: {
+				y: 'boxMark.encoding.minValue',
+				x: 'boxMark.encoding.x'
+			},
+			interactive: false,
+			transition: { disabled: true }
+		}
+	];
 	const loadedData = $derived(DataModel.loadDataSync(data, schema));
 	const [femaleDM, maleDM] = $derived(
 		new DataModel(loadedData)
@@ -30,13 +138,6 @@
 					return index % 50;
 				}
 			)
-			.select({
-				operator: 'and',
-				conditions: [
-					{ field: 'Every 5 years T|F', value: 'True', operator: 'eq' },
-					{ field: 'Country: OECD Avg ', value: 'OECD average', operator: 'neq' }
-				]
-			})
 			.select({ field: 'Gender', value: 'Female', operator: 'eq' }, { mode: 'all' })
 	);
 	const env = $derived(muze());
@@ -51,28 +152,45 @@
 		dataModel,
 		color,
 		showYAxis,
-		colorName
+		colorName,
+		showBox
 	}: {
 		canvas: Canvas;
 		dataModel: TDataModel;
 		color: `#${string}`;
 		showYAxis: boolean;
 		colorName: string;
+		showBox: boolean;
 	}) => {
 		canvas
 			.data(dataModel)
-			.rows(['Effective labour market exit age'])
-			.columns(['Year', 'Jitter'])
+			.rows(
+				showBox
+					? [
+							share(
+								'min_age',
+								'median_age',
+								'max_age',
+								'q1_age',
+								'q3_age',
+								'Effective labour market exit age'
+							)
+						]
+					: ['Effective labour market exit age']
+			)
+			.columns([['Year', showBox ? 'Year' : 'Jitter']])
 			.detail(['Country'])
 			.config({
 				legend: { show: false },
 				axes: {
 					x: {
 						compact: true,
-						show: false
+						show: false,
+						transition: { disabled: true }
 					},
 					y: {
 						show: showYAxis,
+						transition: { disabled: true },
 						name: 'AGE OF LABOUR MARKET EXIT →',
 						domain: [49, 81],
 						tickFormat: ({
@@ -121,16 +239,42 @@
 					}
 				}
 			})
-			.layers([
-				{
-					mark: 'point',
-					encoding: {
-						color: { value: () => color },
-						size: { value: () => 6 }
-					}
-				}
-			]);
+			.layers(
+				showBox
+					? [
+							{
+								mark: 'boxMark',
+								encoding: {
+									minValue: 'min_age',
+									medianValue: 'median_age',
+									x: 'Year',
+									y: 'Effective labour market exit age',
+									color: { value: () => 'transparent' },
+									pointColor: { value: () => color },
+									pointSize: { value: () => 6 },
+									maxValue: 'max_age',
+									quarter: 'q1_age',
+									thirdQuarter: 'q3_age'
+								}
+							}
+						]
+					: [
+							{
+								mark: 'point',
+								encoding: {
+									x: 'Jitter',
+									y: 'Effective labour market exit age',
+									color: { value: () => color },
+									size: { value: () => 6 }
+								}
+							}
+						]
+			);
 	};
+
+	$effect(() => {
+		layerFactory.composeLayers('boxMark', layers);
+	});
 
 	$effect(() => {
 		updateCanvas({
@@ -138,7 +282,8 @@
 			dataModel: femaleDM,
 			color: '#6366f1',
 			showYAxis: true,
-			colorName: 'text-indogo-500'
+			colorName: 'text-indogo-500',
+			showBox: isAdvanced
 		});
 	});
 
@@ -148,7 +293,8 @@
 			dataModel: maleDM,
 			color: '#eab308',
 			showYAxis: false,
-			colorName: 'text-yellow-500'
+			colorName: 'text-yellow-500',
+			showBox: isAdvanced
 		});
 	});
 
@@ -271,17 +417,15 @@
 	const onFemaleRenderComplete = ({ emitter: canvas }: { emitter: Canvas }) => {
 		onRenderComplete({ emitter: canvas });
 
-		canvas.yAxes().forEach((yAxis) => {
-			const axisSVG = yAxis.mount();
-			const axisName = axisSVG.querySelector('text.muze-axis-name');
+		const axisSVG = canvas.yAxes()[0].mount();
+		const axisName = axisSVG.querySelector('text.muze-axis-name');
 
-			if (axisName instanceof SVGTextElement) {
-				axisName.setAttribute(
-					'transform',
-					`matrix(0, -1, 1, 0, -67.2, ${axisSVG.getBoundingClientRect().height - 18})`
-				);
-			}
-		});
+		if (axisName instanceof SVGTextElement) {
+			axisName.setAttribute(
+				'transform',
+				`matrix(0, -1, 1, 0, -67.2, ${axisSVG.getBoundingClientRect().height - 18})`
+			);
+		}
 	};
 
 	$effect(() => {
@@ -319,20 +463,34 @@
 	});
 </script>
 
-<div class="boxplot flex grow gap-4">
+<div class="boxplot flex grow">
 	<div class="flex grow-[545] flex-col">
 		<h2>FEMALE</h2>
-		<div class="not-prose grow" bind:this={femaleViz}></div>
+		<div
+			class="not-prose grow"
+			style:--boxOpacity={boxOpacity}
+			style:--medianOpacity={1 - boxOpacity}
+			bind:this={femaleViz}
+		></div>
 	</div>
 	<div class="flex grow-[455] flex-col">
 		<h2>MALE</h2>
-		<div class="not-prose grow" bind:this={maleViz}></div>
+		<div
+			class="not-prose grow"
+			style:--boxOpacity={boxOpacity}
+			style:--medianOpacity={1 - boxOpacity}
+			bind:this={maleViz}
+		></div>
 	</div>
 </div>
 
 <style>
 	div.boxplot :global(text.muze-axis-name) {
 		text-anchor: start !important;
+	}
+
+	div.boxplot :global(div.muze-layout-container-component:has(div.muze-axis-cell-right)) {
+		display: none !important;
 	}
 
 	div.boxplot :global(div.muze-grid-center td.muze-grid-td) {
@@ -364,5 +522,19 @@
 
 	div.boxplot :global(text.muze-axis-name) {
 		fill: #000000 !important;
+	}
+
+	div.boxplot :global(.median-line) {
+		opacity: var(--medianOpacity);
+	}
+
+	div.boxplot :global(.muze-layer-tick) {
+		stroke: #000000 !important;
+		opacity: var(--boxOpacity);
+	}
+
+	div.boxplot :global(.muze-layer-bar) {
+		stroke: #000000 !important;
+		opacity: var(--boxOpacity);
 	}
 </style>
--- src/routes/viz/boxplot/+page.svelte
+++ src/routes/viz/boxplot/+page.svelte
@@ -6,6 +6,8 @@
 
 	const schema = $derived(pageData.schema);
 	const data = $derived(pageData.data);
+
+	let isAdvanced = $state(false);
 </script>
 
 <main class="prose m-auto flex size-full max-w-4xl flex-col p-4 prose-h1:mb-4 prose-h2:my-2">
@@ -13,7 +15,17 @@
 		DISTRIBUTION OF OECD COUNTRIES BY EFFECTIVE AGE OF LABOUR MARKET EXIT FOR EACH 5 YEARS,
 		1972-2022
 	</h1>
+	<div class="self-end">
+		<label class="align-middle" for="advanced">Unlock insights?</label>
+		<input
+			class="align-middle"
+			type="checkbox"
+			id="advanced"
+			value="surprise"
+			bind:checked={isAdvanced}
+		/>
+	</div>
 	{#await import('@viz/muze') then { default: muze }}
-		<Boxplot {muze} {schema} {data}></Boxplot>
+		<Boxplot {muze} {schema} {data} {isAdvanced}></Boxplot>
 	{/await}
 </main>
